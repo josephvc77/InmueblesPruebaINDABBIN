34566b69998270ca20df176a35b4c2bab62eb3cb
Joseph Valencia <70192127+josephvc77@users.noreply.github.com>
2023-09-29 13:27:07 -0600

    hotfix

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'inmuebles_prueba',
-        'USER': 'root',
-        'PASSWORD': '$Ic0DiDgt1c!',
-        'HOST': '192.168.101.199',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'inmuebles_prueba',
+#         'USER': 'root',
+#         'PASSWORD': '$Ic0DiDgt1c!',
+#         'HOST': '192.168.101.199',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
